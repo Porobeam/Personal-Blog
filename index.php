db6485bd21d8ba27689754184501d8b78ad8e8a5
Porobeam <fzgbeam@gmail.com>
2023-11-29 12:18:31 -0600

    First commit! Not ready for release. Just setting up.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Personal Blog</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>Personal Blog</h1>
+    <nav>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#rss">RSS</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="container">
+    <img src="images/me.png" alt="Iván Pérez">
+    <div class="content">
+      <h2>Porobeam</h2>
+      <h3>About me</h3>
+      <p>Hi, I’m Iván Pérez, a 21-year-old backend developer with experience in PHP Composer and Symphony environments. I’m an avid fan of Linux and servers. I love creating new things and understanding the ones that already exist.</p>
+    </div>
+  </div>
+</body>
+</html>
